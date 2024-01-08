8c71911cc7a8aa168933f5ac648f7fc1c9714bcf
amitaybenami <benami.amitay@gmail.com>
2024-01-08 19:30:49 +0200

    TriangleTests

--- unittests/geometries/TriangleTests.java
+++ unittests/geometries/TriangleTests.java
@@ -35,17 +35,17 @@ class TriangleTests {
     /**Test method for {@link geometries.Triangle#findIntersections(Ray)}*/
     @Test
     void testFindIntersections() {
-        Point p100 = new Point(1,0,0);
-        Point p010 = new Point(0,1,0);
-        Point p110 = new Point(1,1,0);
-        Point p111 = new Point(1,1,1);
-        Point p0_70_70 = new Point(0.7,0.7,0);
-        Vector vM0_3M0_3M1 = new Vector(-0.3,-0.3,-1);
-        Vector vM0_7M0_7M1 = new Vector(-0.7,-0.7,-1);
-        Vector v0_20_2M1 = new Vector(0.2,0.2,-1);
-        Vector vM0_50M1 = new Vector(-0.5,0,-1);
-        Vector v0_50M1 = new Vector(0.5,0,-1);
-        Vector v00M1 = new Vector(0,0,-1);
+        final Point p100 = new Point(1,0,0);
+        final Point p010 = new Point(0,1,0);
+        final Point p110 = new Point(1,1,0);
+        final Point p111 = new Point(1,1,1);
+        final Point p0_70_70 = new Point(0.7,0.7,0);
+        final Vector vM0_3M0_3M1 = new Vector(-0.3,-0.3,-1);
+        final Vector vM0_7M0_7M1 = new Vector(-0.7,-0.7,-1);
+        final Vector v0_20_2M1 = new Vector(0.2,0.2,-1);
+        final Vector vM0_50M1 = new Vector(-0.5,0,-1);
+        final Vector v0_50M1 = new Vector(0.5,0,-1);
+        final  Vector v00M1 = new Vector(0,0,-1);
         Triangle triangle = new Triangle(p010, p110,p100);
         // ============ Equivalence Partitions Tests ==============
         // TC01: Point is inside the triangle (1 point)
