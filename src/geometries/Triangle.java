d8f43af28ca21bc6e2ade696a7f929c5afbf6f82
amitaybenami <benami.amitay@gmail.com>
2024-01-09 16:20:05 +0200

    Triangle.findIntersections

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -2,6 +2,8 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Util;
+import primitives.Vector;
 
 import java.util.List;
 
@@ -25,6 +27,24 @@ public class Triangle extends Polygon {
 
     @Override
     public List<Point> findIntersections(Ray ray) {
+        List<Point> planeIntersection = plane.findIntersections(ray);
+        if (planeIntersection == null)//no intersections with the plane
+            return null;
+        // vectors from ray's head to vertices
+        Vector v1 = vertices.get(0).subtract(ray.getHead());
+        Vector v2 = vertices.get(1).subtract(ray.getHead());
+        Vector v3 = vertices.get(2).subtract(ray.getHead());
+
+        Vector n1 = v1.crossProduct(v2).normalize();
+        Vector n2 = v2.crossProduct(v3).normalize();
+        Vector n3 = v3.crossProduct(v1).normalize();
+
+        //if plane's intersection is inside the triangle
+        if(Util.compareSign(ray.getDirection().dotProduct(n1),ray.getDirection().dotProduct(n2)) &&
+        Util.compareSign(ray.getDirection().dotProduct(n2),ray.getDirection().dotProduct(n3)) &&
+        Util.compareSign(ray.getDirection().dotProduct(n3),ray.getDirection().dotProduct(n1)))
+            return planeIntersection;
+
         return null;
     }
 }
