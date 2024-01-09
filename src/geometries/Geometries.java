cc4bd8a36a9e20d9728a22852331bf5ac4795176
amitaybenami <benami.amitay@gmail.com>
2024-01-09 19:12:41 +0200

    GeometriesTests and Geometries declaration

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,44 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Ray;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * this class is used to create a list of geometries
+ * @author Elad and Amitay
+ */
+public class Geometries implements Intersectable{
+    //the list itself
+    private final List<Intersectable> geometries = new LinkedList<Intersectable>();
+
+    /**
+     * default constructor does nothing
+     */
+    public Geometries() {
+    }
+
+    /**
+     * constructor that gets array of geometries and assign it as the list
+     * @param geometries an array of geometries
+     */
+    public Geometries(Intersectable... geometries){
+        add(geometries);
+    }
+
+    /**
+     * gets an array of geometries and ass it to the list
+     * @param geometries an array of geometries
+     */
+    public void add(Intersectable... geometries){
+        this.geometries.addAll(List.of(geometries));
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        return null;
+    }
+
+}
